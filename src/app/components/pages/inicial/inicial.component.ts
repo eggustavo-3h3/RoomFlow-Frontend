4ea776c69e1035ba1525523238243731cec378bf
Allcantaraa <kauayt120@gmail.com>
2025-04-29 21:41:04 -0300

    feat: responsividade para mobile na tela inicial

--- src/app/components/pages/inicial/inicial.component.ts
+++ src/app/components/pages/inicial/inicial.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AngularMaterialModule } from '../../../angular-material/angular-material.module';
 
 @Component({
   selector: 'app-inicial',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, AngularMaterialModule],
   templateUrl: './inicial.component.html',
   styleUrl: './inicial.component.css'
 })
-export class InicialComponent {
+export class InicialComponent implements OnInit {
+
+  isMobile = false;
+
+  ngOnInit(): void {
+    this.checkScreenSize();
+    window.addEventListener('resize', () => this.checkScreenSize());
+  }
+
+  checkScreenSize() {
+    this.isMobile = window.innerWidth <= 768;
+  }
 
 }
